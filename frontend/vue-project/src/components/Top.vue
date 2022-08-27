3f61ed6f22fc36e68f920fdd75eb172de47eba74
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-08-27 13:34:27 +0900

    succeeded to connect

---
+++ frontend/vue-project/src/components/Top.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const connect = async() => {
+	const accouts = await window.ethereum.request({method: "eth_requestAccounts"})
+	console.log(accouts[0]);
+};
+</script>
+
+<template>
+	<button @click="connect">Connect Metamask</button>
+</template>
