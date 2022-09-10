2d8262a16fe8b9f1c94317b64306c59ee370967a
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-09-10 12:13:07 +0900

    succeeded to safeBatchTransferFrom

--- frontend/vue-project/src/components/Top.vue
+++ frontend/vue-project/src/components/Top.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-import { ethers } from 'ethers';
+import { BigNumber, ethers } from 'ethers';
 import gameitemsabi from "@/abi/gameitems.json";
 import detectEthereumProvider from '@metamask/detect-provider';
 import type { GameItems } from "@/abi/myERC1155.sol/index";
 // const provider: any = await detectEthereumProvider();
+
+const contractAddress = "0xc06380D2867F3Ca42A0DA122643A41455dB2A9B6";
+const deployerAddress = "0x32a9E70324862ef7BF8bA7610AF701822ddE5364";
+const playerAddress = "0x3D896D141dC4eEe51E829CcA7003939be20c280A";
+
 
 const connect = async() => {
 	const accouts = await window.ethereum.request({method: "eth_requestAccounts"})
@@ -12,24 +17,48 @@ const connect = async() => {
 const getName = async() => {
 	const provider = new ethers.providers.AlchemyProvider("goerli")
 	console.log(provider)
-	const gameItemsContract  = await new ethers.Contract("0xc06380D2867F3Ca42A0DA122643A41455dB2A9B6", gameitemsabi, provider) as GameItems;
-	const GOLD = await gameItemsContract.GOLD()
+	const gameItemsContract  = await new ethers.Contract(contractAddress, gameitemsabi, provider) as GameItems;
+	const GOLD = await (await gameItemsContract.GOLD()).toString()
 	console.log(GOLD);
 }
 const getUri = async() => {
 	const provider = new ethers.providers.AlchemyProvider("goerli")
-	const gameItemsContract: GameItems = await new ethers.Contract("0xc06380D2867F3Ca42A0DA122643A41455dB2A9B6", gameitemsabi, provider)  as GameItems;
+	const gameItemsContract: GameItems = await new ethers.Contract(contractAddress, gameitemsabi, provider)  as GameItems;
 	const uri = await gameItemsContract.uri(2)
 }
 const getDeployerBalanceOf = async() => {
 	const provider = new ethers.providers.AlchemyProvider("goerli")
-	const gameItemsContract: GameItems = await new ethers.Contract("0xc06380D2867F3Ca42A0DA122643A41455dB2A9B6", gameitemsabi, provider)  as GameItems;
-	const address = "0x32a9E70324862ef7BF8bA7610AF701822ddE5364"
-	const balanceHex = await gameItemsContract.balanceOf(address,2)
-	const balance = parseInt(String(balanceHex), 16)
+	const gameItemsContract: GameItems = await new ethers.Contract(contractAddress, gameitemsabi, provider)  as GameItems;
+	const address = deployerAddress
+	const balanceHex = await gameItemsContract.balanceOf(address,0)
+	const balance = balanceHex.toString()
 	console.log(balance)
+	// const balance = parseInt(String(balanceHex), 16)
+	// console.log(financial(balance))
 	// await gameItemsContract.
 	// const uri = await gameItemsContract.uri(2)
+}
+const getBalanceOfBatch = async() => {
+	const provider = new ethers.providers.AlchemyProvider("goerli")
+	const gameItemsContract: GameItems = await new ethers.Contract(contractAddress, gameitemsabi, provider)  as GameItems;
+	const address = deployerAddress
+	const balanceHex = await gameItemsContract.balanceOfBatch([address, address, address, address, address], [1, 2, 3, 4, 5])
+	// const balance = balanceHex.map(hex => parseInt(String(hex), 16));
+	// console.log(balanceHex)
+	// await gameItemsContract.
+	// const uri = await gameItemsContract.uri(2)
+}
+const financial = (x:number) => {
+  return Number.parseFloat(String(x)).toFixed(2);
+}
+const safeBatchTransferFrom = async () => {
+	// const provider = new ethers.providers.AlchemyProvider("goerli")
+	const provider = new ethers.providers.Web3Provider(window.ethereum)
+	const signer = await provider.getSigner();
+	const gameItemsContract: GameItems = await new ethers.Contract(contractAddress, gameitemsabi, signer)  as GameItems;
+	const res = await gameItemsContract.safeBatchTransferFrom(deployerAddress, playerAddress, [0, 1, 3, 4], [50, 100, 1, 1], "0x00").catch(err => {
+		console.error(err)
+	})
 }
 </script>
 
@@ -38,4 +67,6 @@ const getDeployerBalanceOf = async() => {
 	<button @click="getName">getName</button>
 	<button @click="getUri">getUri</button>
 	<button @click="getDeployerBalanceOf">getDeployerBalanceOf</button>
+	<button @click="getBalanceOfBatch">getBalanceOfBatch</button>
+	<button @click="safeBatchTransferFrom">safeBatchTransferFrom</button>
 </template>
